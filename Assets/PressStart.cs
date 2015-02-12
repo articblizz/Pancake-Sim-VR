cb5d87ac44592071cb1774c84946a8f2852c8662
Felix Östh <felixosth@gmail.com>
2015-02-12 10:07:49 +0100

    Added existing project

---
+++ Assets/PressStart.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressStart : MonoBehaviour {
+
+
+    bool isPaused = true;
+
+    public KeppindaScore score;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                score.Resume();
+            else
+                score.Pause();
+            isPaused = !isPaused;
+        }
+	}
+}
